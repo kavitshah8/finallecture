d99361842acae609f1d11e2b9e7286f706322e18
kavitshah8 <kavit.shah8@gmail.com>
2015-02-04 23:48:19 -0500

    server side code for dummy authentication

--- server/index.js
+++ server/index.js
@@ -1,20 +1,16 @@
-// To use it create some files under `mocks/`
-// e.g. `server/mocks/ember-hamsters.js`
-//
-// module.exports = function(app) {
-//   app.get('/ember-hamsters', function(req, res) {
-//     res.send('hello');
-//   });
-// };
-
 module.exports = function(app) {
   var globSync   = require('glob').sync;
   var mocks      = globSync('./mocks/**/*.js', { cwd: __dirname }).map(require);
   var proxies    = globSync('./proxies/**/*.js', { cwd: __dirname }).map(require);
+  var bodyParser = require('body-parser');    
 
   // Log proxy requests
   var morgan  = require('morgan');
   app.use(morgan('dev'));
+
+  // Contains key-value pairs of data submitted in the request body.
+	app.use(bodyParser.json()); // for parsing application/json
+	app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
   mocks.forEach(function(route) { route(app); });
   proxies.forEach(function(route) { route(app); });
--- server/mocks/server.js
+++ server/mocks/server.js
@@ -9,6 +9,7 @@ module.exports = function(app) {
   });
 
   serverRouter.post('/', function(req, res) {
+    console.log("Hello");
     res.status(201).end();
   });
 
@@ -33,4 +34,44 @@ module.exports = function(app) {
   });
 
   app.use('/api/server', serverRouter);
+
+  // client-side authentication
+  var currentToken;
+
+  app.post('/auth.json', function(req, res) {
+
+    var body = req.body,
+        username = body.username,
+        password = body.password;
+
+    if (username === 'root' && password === 'root') {
+      currentToken = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+      res.send({
+        success: true,
+        token: currentToken 
+      });
+    } else {
+      res.send({
+        success: false,
+        message: 'Invalid username/password' 
+      });
+    }
+
+  });
+
+  function validTokenProvided(req, res) {
+
+    var userToken = req.body.token || req.param('token') || req.headers.token;
+
+    if (!currentToken || userToken != currentToken) {
+      res.status(401).send({ 
+        error: 'Invalid token. You provided: ' + userToken + ' token.'
+      });
+      
+      return false;
+    }
+
+    return true;
+  }
+
 };
